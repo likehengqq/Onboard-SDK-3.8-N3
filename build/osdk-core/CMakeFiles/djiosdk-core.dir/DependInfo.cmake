5a453e05d678b98ab80ae8323111f2f306b7bdb7
like <13551400929@163.com>
2021-03-03 22:48:28 +0800

    修改了指点飞行方式

---
+++ build/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_ack.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_ack.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_broadcast.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_broadcast.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_camera.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_camera.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_command.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_command.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_control.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_control.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_error.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_error.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_gimbal.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_gimbal.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_hardware_sync.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_hardware_sync.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_hotpoint.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_hotpoint.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_mfio.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_mfio.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_mission_manager.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_mission_manager.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_mobile_communication.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_mobile_communication.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_mobile_device.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_mobile_device.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_payload_device.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_payload_device.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_subscription.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_subscription.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_vehicle.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_vehicle.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_version.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_version.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_virtual_rc.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_virtual_rc.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/api/src/dji_waypoint.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/api/src/dji_waypoint.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/hal/src/dji_hard_driver.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/hal/src/dji_hard_driver.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/hal/src/dji_log.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/hal/src/dji_log.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/hal/src/dji_memory.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/hal/src/dji_memory.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/hal/src/dji_platform_manager.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/hal/src/dji_platform_manager.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/hal/src/dji_thread_manager.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/hal/src/dji_thread_manager.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/platform/default/src/dji_memory_default.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/platform/default/src/dji_memory_default.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/platform/linux/src/linux_serial_device.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/platform/linux/src/linux_serial_device.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/platform/linux/src/posix_thread.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/platform/linux/src/posix_thread.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/platform/linux/src/posix_thread_manager.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/platform/linux/src/posix_thread_manager.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/protocol/src/dji_aes.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/protocol/src/dji_aes.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/protocol/src/dji_open_protocol.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/protocol/src/dji_open_protocol.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/protocol/src/dji_protocol_base.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/protocol/src/dji_protocol_base.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/utility/src/dji_circular_buffer.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/utility/src/dji_circular_buffer.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/osdk-core/utility/src/dji_singleton.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/utility/src/dji_singleton.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../osdk-core/api/inc"
+  "../osdk-core/protocol/inc"
+  "../osdk-core/hal/inc"
+  "../osdk-core/utility/inc"
+  "../osdk-core/platform/default/inc"
+  "../osdk-core/platform/linux/inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amov/下载/Onboard-SDK-3.8/sample/linux/common/dji_linux_environment.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/__/common/dji_linux_environment.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/sample/linux/common/dji_linux_helpers.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/__/common/dji_linux_helpers.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/sample/linux/payload-3rd-party/main.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/main.cpp.o"
+  "/home/amov/下载/Onboard-SDK-3.8/sample/linux/payload-3rd-party/payload-control-sample.cpp" "/home/amov/下载/Onboard-SDK-3.8/build/sample/linux/payload-3rd-party/CMakeFiles/djiosdk-payload-control-sample.dir/payload-control-sample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../sample/linux/../../osdk-core/api/inc"
+  "../sample/linux/../../osdk-core/utility/inc"
+  "../sample/linux/../../osdk-core/hal/inc"
+  "../sample/linux/../../osdk-core/protocol/inc"
+  "../sample/linux/../../osdk-core/platform/linux/inc"
+  "../sample/linux/payload-3rd-party/../common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/amov/下载/Onboard-SDK-3.8/build/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
